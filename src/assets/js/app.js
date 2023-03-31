014183bb7d96b6e393a939ef4aa66f425bdc8a25
defycab <love.bellinder@gmail.com>
2023-03-31 03:19:47 +0200

    added classes and ids for elements. styling changes to css file

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,44 +1,44 @@
 const storage = window.localStorage
 
 const renderContacts = () => {
-  const contacts = JSON.parse(storage.getItem('contacts'))
+  const contacts = JSON.parse(storage.getItem("contacts"))
 
-  let div = document.querySelector('#contact-list')
+  let div = document.querySelector("#contact-list")
   if (contacts) {
-    div.innerHTML = ''
-    const ul = document.createElement('ul')
+    div.innerHTML = ""
+    const ul = document.createElement("ul")
 
     contacts.forEach((contact) => {
-      let li = document.createElement('li')
+      let li = document.createElement("li")
 
       li.innerHTML = `
-		  <span>${contact.name}</span> |
-		  <span>${contact.email}</span> |
-		  <span>${contact.phone}</span>
+		  <span class="contact-display">${contact.name}</span> |
+		  <span class="contact-display">${contact.email}</span> |
+		  <span class="contact-display">${contact.phone}</span>
 	    `
       ul.appendChild(li)
     })
 
     div.appendChild(ul)
   } else {
-    div.innerHTML = '<p>You have no contacts in your address book</p>'
+    div.innerHTML = "<p>You have no contacts in your address book</p>"
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   renderContacts()
-  const contactForm = document.getElementById('new-contact-form')
-  const toggleFormVisibilityButton = document.getElementById('add-contact')
-  contactForm.style.display = 'none'
+  const contactForm = document.getElementById("new-contact-form")
+  const toggleFormVisibilityButton = document.getElementById("add-contact")
+  contactForm.style.display = "none"
 
-  toggleFormVisibilityButton.addEventListener('click', () => {
-    if (contactForm.style.display === '') {
-      contactForm.style.display = 'none'
+  toggleFormVisibilityButton.addEventListener("click", () => {
+    if (contactForm.style.display === "") {
+      contactForm.style.display = "none"
     } else {
-      contactForm.style.display = ''
+      contactForm.style.display = ""
     }
   })
-  contactForm.addEventListener('submit', (event) => {
+  contactForm.addEventListener("submit", (event) => {
     event.preventDefault()
 
     // 1. Read all the input fields and get their values
@@ -53,12 +53,12 @@ document.addEventListener('DOMContentLoaded', () => {
       twitter: twitter.value,
     }
 
-    let contacts = JSON.parse(storage.getItem('contacts')) || []
+    let contacts = JSON.parse(storage.getItem("contacts")) || []
 
     contacts.push(contact)
 
     // 2. Save them to our storage
-    storage.setItem('contacts', JSON.stringify(contacts))
+    storage.setItem("contacts", JSON.stringify(contacts))
     renderContacts()
     contactForm.reset()
   })
